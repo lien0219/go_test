40d37fe920eb5d772e4b7363ee20a46dc6a21ac5
lien0219 <lien0001@163.com>
2024-09-18 23:56:04 +0800

    算法：单链表的添加和显示，按排序插入

--- src/algorithm/singleLink/main.go
+++ src/algorithm/singleLink/main.go
@@ -9,7 +9,7 @@ type HeroNode struct {
 	next     *HeroNode //指向下一个节点
 }
 
-// 链表插入节点
+// 1.链表插入节点
 func InsertHeroNode(head *HeroNode, newHeroNode *HeroNode) {
 	temp := head
 	for {
@@ -22,6 +22,31 @@ func InsertHeroNode(head *HeroNode, newHeroNode *HeroNode) {
 	//新节点加入链表最后
 	temp.next = newHeroNode
 
+}
+
+// 2.链表插入节点，根据no的编号从大到小插入
+func InsertHeroNode2(head *HeroNode, newHeroNode *HeroNode) {
+	temp := head
+	flag := true
+	for {
+		if temp.next == nil { //链表最后
+			break
+		} else if temp.next.no < newHeroNode.no {
+			//newHeroNode插在temp后
+			break
+		} else if temp.next.no == newHeroNode.no {
+			//链表已经有此no就不插入
+			flag = false
+			break
+		}
+	}
+	if !flag {
+		fmt.Println("已经存在no:", newHeroNode.no)
+		return
+	} else {
+		newHeroNode.next = temp.next
+		temp.next = newHeroNode
+	}
 }
 
 // 显示链表的所有信息
@@ -56,10 +81,19 @@ func main() {
 		name:     "如来",
 		nickname: "佛祖",
 	}
+	head3 := &HeroNode{
+		no:       3,
+		name:     "燃灯",
+		nickname: "大佛",
+	}
 
 	//加入
-	InsertHeroNode(head, head1)
-	InsertHeroNode(head, head2)
+	//InsertHeroNode(head, head1)
+	//InsertHeroNode(head, head2)
+	//InsertHeroNode(head, head3)
+	InsertHeroNode2(head, head1)
+	InsertHeroNode2(head, head2)
+	InsertHeroNode2(head, head3)
 	//显示
 	ListHeroNode(head)
 }
