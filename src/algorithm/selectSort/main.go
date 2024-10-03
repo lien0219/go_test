b929aa13da89e43e29d70367dff61a5ddcc4226d
lien0219 <lien0001@163.com>
2024-10-03 14:44:02 +0800

    算法：选择排序耗时

--- src/algorithm/selectSort/main.go
+++ src/algorithm/selectSort/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // 排序函数
-func SelectSort(arr *[5]int) {
+func SelectSort(arr *[80000]int) {
 	//(*arr)[1] = 600
 	//将最大值和arr[0]交换
 	for j := 0; j < len(arr)-1; j++ {
@@ -19,12 +23,19 @@ func SelectSort(arr *[5]int) {
 		if maxIndex != j {
 			arr[j], arr[maxIndex] = arr[maxIndex], arr[j]
 		}
-		fmt.Printf("第%d次：%v \n", j+1, *arr)
+		//fmt.Printf("第%d次：%v \n", j+1, *arr)
 	}
 }
 func main() {
-	arr := [5]int{10, 34, 19, 100, 80}
+	var arr [80000]int
+	for i := 0; i < 80000; i++ {
+		arr[i] = rand.Intn(900000)
+	}
+	//arr := [5]int{10, 34, 19, 100, 80}
+	start := time.Now().Unix()
 	SelectSort(&arr)
+	end := time.Now().Unix()
+	fmt.Printf("选择排序耗时：%d \n", end-start)
 	fmt.Println("main函数")
-	fmt.Println(arr)
+	//fmt.Println(arr)
 }
